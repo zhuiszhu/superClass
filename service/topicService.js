9d11f0646a0d2b29376886c3118a643434593c3d
zhuiszhu <1799150010@qq.com>
2017-05-10 20:02:47 +0800

    完成教师端添加题目功能,添加题目后实现插入学员题目关系列表

--- service/topicService.js
+++ service/topicService.js
@@ -1,6 +1,7 @@
 var DBCon = require("../db/DBconnect.js");
 var userDB = new DBCon("users");
 var topicDB = new DBCon("topic");
+var sTTDB = new DBCon("studentToTopic");
 var event = require("../functions/publicEvent");
 var getSocketUser = require("../service/socketService");
 
@@ -15,6 +16,7 @@ var topicService = {
         if (req.session.userObj && req.session.userObj[0].type == 0){//讲师身份,可以添加问题
             var userObj = req.session.userObj[0];
             var topicObj = req.body;
+            var topicID = null;
             topicObj.title = topicObj.title.trim();
             topicObj.content = topicObj.content.trim();
             topicObj.fraction = topicObj.fraction*1;
@@ -37,10 +39,37 @@ var topicService = {
 
                 event.removeAllListeners("DB_OOP_SUCCESS");
                 event.on("DB_OOP_SUCCESS" , data => {
-                    sendObj.aut = true;
-                    sendObj.txt = "添加成功";
 
-                    res.json(sendObj);
+                    if(data.collection == "topic" && data.oop == "insert"){//对题目表的插入操作成功
+
+                        topicID = data.info.insertedIds[0];
+
+
+                        userDB.find({class : userObj.class , type : "1"},{_id : 1});
+
+                    }else if(data.collection == "users" && data.oop == "find"){//对用户列表的查询操作成功
+                        // console.log(data.info);
+                        var userList = data.info;
+                        if(topicID != null){//题目添加成功了才能添加关系表内容
+                            var dataList = userList.map(function(item , index){
+                                return {
+                                    studentID : item._id,
+                                    result : 0,
+                                    topicID : topicID,
+                                    replyTime : null,
+                                    class : userObj.class,
+                                    fraction : topicObj.fraction,
+                                    state : false
+                                }
+                            });
+                            sTTDB.insert(dataList);
+                        }
+                    }else if(data.collection == "studentToTopic" && data.oop == "insert"){//学员题目关系表数据插入成功
+                        sendObj.aut = true;
+                        sendObj.txt = "添加成功";
+                        res.json(sendObj);
+                        
+                    }
                 });
 
                 topicDB.insert(topicObj);
